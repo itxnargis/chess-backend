c2fc342076c87aef8e5fb66dbe9d00ca2c041c9b
itxnargis <itxnargiskhatun@gmail.com>
2025-03-10 19:15:17 +0530

    fix - Server error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const httpServer = createServer(app)
 
 // Allow requests from all origins in production
 const corsOptions = {
-  origin: process.env.NODE_ENV === "production" ? true : ["https://chess-frontend-dun.vercel.app", "http://localhost:5173"],
+  origin: "https://chess-frontend-dun.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }
